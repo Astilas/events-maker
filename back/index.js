5bccb9a6478d34e344c2eeb97bd4f86283e0f07c
Thibault <thibault@maarkt.io>
2020-03-02 10:57:22 +0100

    fix CORS issue

--- back/index.js
+++ back/index.js
@@ -11,10 +11,11 @@ app.use(
     extended: true,
   })
 )
-
-// app.get('/', (request, response) => {
-//   response.json({ info: 'Node.js, Express, and Postgres API' })
-// })
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Routes
 app.get('/users', db.getUsers);
